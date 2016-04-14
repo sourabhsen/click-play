d4e3020cbe051fb89bfc782d5c4596468d7fefa0
Sourabh Sen <Sourabh.Sen@RealPage.com>
2016-04-14 16:00:50 +0530

    first commit

---
+++ src/js/controllers/signin.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/* Controllers */
+  // signin controller
+app.controller('SigninFormController', ['$scope', '$http', '$state','AuthenticationService', function($scope, $http, $state,AuthenticationService) {
+    $scope.user = {};
+    $scope.authError = null;
+    
+    function init(){
+      // reset login status
+      AuthenticationService.ClearCredentials();
+    };
+
+    $scope.login = function() {
+      $scope.authError = null;
+      // Try to login
+     /* $http.post('api/login.json', {email: $scope.user.email, password: $scope.user.password})
+      .then(function(response) {
+        if ( !response.data.user ) {
+          $scope.authError = 'Email or Password not right';
+        }else{
+          $state.go('app.dashboard-v1');
+        }
+      }, function(x) {
+        $scope.authError = 'Server Error';
+      });*/
+      
+      AuthenticationService.Login($scope.user.email, $scope.user.password, function (response) {
+            if (response.success) {
+                AuthenticationService.SetCredentials($scope.user.email, $scope.user.password);
+                 $state.go('app.dashboard-v1');
+            } else {
+                  $scope.authError = 'Server Error';
+                  vm.dataLoading = false;
+            }
+        });
+
+    };
+   
+    init();
+
+  }])
+;
---
+++ src/js/services/user.service.js
@@ -0,0 +1,71 @@
+
+(function () {
+ 
+ 'use strict';
+
+app.factory('UserService', UserService);
+	
+    UserService.$inject = ['$http','$q','environmentUtil'];
+    function UserService($http,$q,environmentUtil) {
+        var service = {};
+ 
+      
+       
+        service.GetByUsername = GetByUsername;
+        service.Create = Create;
+        service.Update = Update;
+        service.Delete = Delete;
+ 
+        return service;
+ 
+    
+        function GetByUsername(uname,pkey) {
+                var deferredGet = $q.defer();
+                var restUrl = '/clickandpay/j_spring_security_check';
+                var data = {
+                    username: uname,
+                    psword:    pkey
+                };   
+
+                var httpConfig = {
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+                    method: 'POST',
+                    params: data,
+                    url: environmentUtil.getMCAppUrl() + restUrl
+                };
+
+                $http(httpConfig).success(function (data) {
+                    deferredGet.resolve(data);
+                }).error(function (error) {
+                    deferredGet.reject(error);
+                });
+
+                return deferredGet.promise;
+        }
+ 
+        function Create(user) {
+            return $http.post('/api/users', user).then(handleSuccess, handleError('Error creating user'));
+        }
+ 
+        function Update(user) {
+            return $http.put('/api/users/' + user.id, user).then(handleSuccess, handleError('Error updating user'));
+        }
+ 
+        function Delete(id) {
+            return $http.delete('/api/users/' + id).then(handleSuccess, handleError('Error deleting user'));
+        }
+ 
+        // private functions
+ 
+        function handleSuccess(res) {
+            return res.data;
+        }
+ 
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+    }
+ 
+})();
